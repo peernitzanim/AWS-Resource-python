6f7c17ea22bd51594f387f168218894d19cc96b5
peernitzanim <peer.vetzler@nitzanim.tech>
2024-09-18 23:27:01 +0300

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -5,7 +5,8 @@ import route53
 
 def ec2_create(name, myname, ami, instance_type):
     jenkins_info = {'action': 'create', 'name': name, 'myname': myname, 'ami': ami, 'instance_type': instance_type}
-    print(action_jenkins_ec2(jenkins_info))
+    print(jenkins_info)
+    # print(action_jenkins_ec2(jenkins_info))
 
 
 def ec2_list(myname):
